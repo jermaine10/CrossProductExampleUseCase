5de02acc2f0c589d6940bd91f15a7131152b0baf
jermaine10 <jermaine9310@gmail.com>
2019-10-01 23:46:25 +0900

    Initial commit
    created 3d rotation by utilizing cross products
    and shader graph for waves

---
+++ Assets/Script/RotationUsingCross.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationUsingCross : MonoBehaviour
+{
+    public Transform Target;
+    public Transform lookDirection;
+     Rigidbody curRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curRigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var curLook = lookDirection.position - this.transform.position;
+        var view = Target.transform.position - curLook;
+        var direction = curLook / curLook.magnitude;
+
+        var rotationAxis = Vector3.Cross(direction, view);
+        var rotationAngle = (float)Mathf.Acos(Vector3.Dot(direction, view) / direction.magnitude / view.magnitude);
+        this.transform.Rotate(rotationAxis, rotationAngle);
+        Debug.DrawLine(lookDirection.position, Target.position);
+;    }
+}
